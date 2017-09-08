c141eb9c4804b61b70b45a880a01080092e02c4e
t-jack <31381318+t-jack@users.noreply.github.com>
2017-09-09 00:44:25 +0900

    Update time.py

--- time.py
+++ time.py
@@ -3,10 +3,10 @@ from twython import Twython
 from datetime import datetime
 
 # set twitter account
-CONSUMER_KEY = 'sDfbKAwU5wkokdLwVYNr3ZjaX'
-CONSUMER_SECRET = 'ZYub8Jk6jqpKmz5T4Ljn7gicNNtHaTcy8RjCKOYiPCvmP19GKJ'
-ACCESS_KEY = '351582959-EO5JC4JZzIzWbTCCQDmRkHoTppbXZPf7piheMqiI'
-ACCESS_SECRET = '6SqQfl91NZx8sbBObCdofhwAMBUrDHhwzCauAmFeV5bQW'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
 # get time string
